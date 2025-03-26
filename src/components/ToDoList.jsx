ef4885f37c4f1f65fbbe8e92098a8895b37ec6ef
vickyy234 <vickymayilsamy@gmail.com>
2025-03-26 20:45:03 +0530

    Added completion Feature

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,7 +4,7 @@ import '../styles/ToDoList.css'
 function ToDoList() {
     const [tasks, setTasks] = useState([]);
     const [task, setTask] = useState('');
-    const [flag, setFlag] = useState(0);
+    const [editMode, setEditMode] = useState(0);
     const [editindex, setEditIndex] = useState(null);
 
     function handleChanges(event) {
@@ -17,12 +17,12 @@ function ToDoList() {
             return;
         }
         if (editindex !== null) {
-            [tasks[editindex]] = [task];
+            tasks[editindex].text = task;
             setEditIndex(null);
-            setFlag(0);
+            setEditMode(0);
         }
         else {
-            setTasks([...tasks, task]);
+            setTasks([...tasks, { text: task, Completed: false }]);
         }
 
         setTask('');
@@ -34,8 +34,8 @@ function ToDoList() {
     }
 
     function editTask(index) {
-        setFlag(1);
-        setTask(tasks[index]);
+        setEditMode(1);
+        setTask(tasks[index].text);
         setEditIndex(index);
     }
 
@@ -63,6 +63,11 @@ function ToDoList() {
         setTasks([]);
     }
 
+    function toggleCompleted(index) {
+        tasks[index].Completed = !tasks[index].Completed;
+        setTasks([...tasks]);
+    }
+
     return (
         <div className='container'>
             <h1>To-Do-List</h1>
@@ -73,15 +78,15 @@ function ToDoList() {
                     value={task}
                     onChange={handleChanges}
                 />
-                {flag === 1 ? <button className='btn' onClick={addTask}>Update Task</button> : <button className='btn' onClick={addTask}>Add Task</button>}
+                {editMode === 1 ? <button className='btn' onClick={addTask}>Update Task</button> : <button className='btn' onClick={addTask}>Add Task</button>}
             </div>
 
             {tasks.length === 0 ? <h3>No Task Added!</h3> : null}
 
             <ol>
                 {tasks.map((item, index) => (
-                    <li key={index}>
-                        <span className="task-content">{item}</span>
+                    <li key={index} style={{ backgroundColor: item.Completed ? '#99d7a8' : '#4A90E2' }}>
+                        <span className="task-content" onClick={() => { toggleCompleted(index) }} style={{ cursor: 'pointer' }}>{item.text}{item.Completed ? '✅' : null} </span>
                         <div className="task-actions">
                             <button onClick={() => editTask(index)}>✏️</button>
                             <button onClick={() => moveUp(index)}>🔼</button>
